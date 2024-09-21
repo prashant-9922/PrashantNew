5f6d1127b3d3d0e817fffa44e03bb7d13f6872df
PrashantDada <prashant.warule01@gmail.com>
2024-09-21 19:06:00 +0530

    logger added

--- src/test/java/steps/LoginPageStepDef.java
+++ src/test/java/steps/LoginPageStepDef.java
@@ -5,11 +5,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.*;
 import org.testng.Assert;
 import page.LoginPage;
 
 import java.sql.DriverManager;
 import java.util.List;
+
 import factory.DriverFactory;
 
 public class LoginPageStepDef {
@@ -17,6 +19,8 @@ public class LoginPageStepDef {
     String title;
 
     LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+
+    public static Logger logger = LogManager.getLogger(LoginPageStepDef.class);
 
     @When("open login page")
     public void openLoginPage(DataTable dataTable) {
@@ -27,13 +31,14 @@ public class LoginPageStepDef {
     @And("get title of fb page")
     public void getTitleOfFbPage() {
         title = loginPage.getTitle();
-        System.out.println("Fb login page title is " + title);
+        logger.info("Fb login page title is " + title);
     }
 
     @Then("verify login page is opened successfully")
     public void verifyLoginPageIsOpenedSuccessfully() {
         Assert.assertEquals(title,"Log in to Facebook");
-        System.out.println("Fb login page open successfully");
+        logger.info("Fb login page open successfully");
         loginPage.closeBrowser();
+        logger.info("Browser close successfully");
     }
 }
